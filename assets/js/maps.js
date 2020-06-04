d0a99308065d7ee944e7251d0e53bb1b8c2e30df
jaya bidwai <jayab2010@gmail.com>
2020-06-04 05:14:32 +0000

    Initial commit

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 42.358995, lng: -71.049936 },
+        { lat: 42.378106, lng: -71.118800 },
+        { lat: 42.360084, lng: -71.094168 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
